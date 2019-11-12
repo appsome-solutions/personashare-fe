ec1f3927313e12d23c21e762e810ea4262c99e12
Lukasz Mazurowski <36774684+lmazurowski@users.noreply.github.com>
2019-11-12 08:41:05 +0100

    Initial commit

---
+++ src/global/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,28 @@
+import { normalize } from 'styled-normalize';
+import { createGlobalStyle } from 'styled-components';
+import { ThemeInterface } from 'typings';
+
+import 'antd/dist/antd.min.css';
+
+export const GlobalStyles = createGlobalStyle<ThemeInterface>`
+  ${normalize}
+  
+  @font-face {
+    font-family: Roboto, sans-serif;
+    src: url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+  }
+  
+  html {
+    font-family: Roboto, sans-serif;
+  }
+  
+  body {
+    color: ${props => props.theme.colors.utils.text.dark};
+  }
+  
+  a {
+    color: ${props => props.theme.colors.utils.link};
+    text-decoration: none;
+  }
+  
+`;
