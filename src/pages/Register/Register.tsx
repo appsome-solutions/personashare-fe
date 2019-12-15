977ecc7a28f80ac168e392e9cf3193a6de41ec25
Patryk Janik <patryk.janik@castingnetworks.com>
2019-12-15 20:32:09 +0100

    Added persona-share logo and background color.

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { TopNav } from 'components/TopNav/TopNav';
+import LogoSvg from 'assets/logo.svg';
+import styled from 'styled-components';
+
+const StyledLogo = styled.img`
+  margin-top: 46px;
+  margin-bottom: 36px;
+`;
+
+const PageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: ${props => props.theme.colors.utils.background.mid};
+`;
 
 export const Register = () => {
   return (
     <div>
       <TopNav isWithBackArrow />
+      <PageWrapper>
+        <StyledLogo src={LogoSvg} alt="logo" />
+      </PageWrapper>
     </div>
   );
 };
