8eb646daee724125cc0366c228a33cceae677577
pumanitro <patrykjanik1710@gmail.com>
2020-06-03 17:22:44 +0200

    Handled private routes.

--- src/global/AppRouter/AppRouter.tsx
+++ src/global/AppRouter/AppRouter.tsx
@@ -36,21 +36,21 @@ export const AppRouter: FunctionComponent = () => {
     <Switch>
       <Route path={APP_ROUTES.ROOT} exact component={Home} />
       <Route path={APP_ROUTES.SCANNER} exact component={() => <QrScanner onCode={(res) => alert(res.data)} />} />
-      <Route path={APP_ROUTES.MY_SPOTS} exact component={MySpots} />
-      <Route path={APP_ROUTES.CHOOSE_PERSONA} exact component={ChoosePersona} />
-      <Route path={APP_ROUTES.MY_PERSONAS} exact component={MyPersona} />
-      <Route path={APP_ROUTES.MY_SPOTS} exact component={MySpots} />
+      <PrivateRoute path={APP_ROUTES.MY_SPOTS} exact component={MySpots} />
+      <PrivateRoute path={APP_ROUTES.CHOOSE_PERSONA} exact component={ChoosePersona} />
+      <PrivateRoute path={APP_ROUTES.MY_PERSONAS} exact component={MyPersona} />
+      <PrivateRoute path={APP_ROUTES.MY_SPOTS} exact component={MySpots} />
       <Route path={APP_ROUTES.INFORMATIVE_CLAUSE} exact component={InformativeClause} />
       <Route path={APP_ROUTES.PRIVACY_AND_COOKIES_POLICY} exact component={PrivacyAndCookies} />
       <Route path={APP_ROUTES.TERM_OF_USE} exact component={TermOfUse} />
-      <Route path={APP_ROUTES.EDIT_SPOT_UUID_STEP_1(':uuid')} exact component={EditSpotCard} />
-      <Route path={APP_ROUTES.EDIT_PERSONA_UUID_STEP_1(':uuid')} exact component={EditPersonaCard} />
-      <Route path={APP_ROUTES.EDIT_SPOT_UUID_STEP_2(':uuid')} exact component={EditSpotPage} />
-      <Route path={APP_ROUTES.EDIT_PERSONA_UUID_STEP_2(':uuid')} exact component={EditPersonaPage} />
+      <PrivateRoute path={APP_ROUTES.EDIT_SPOT_UUID_STEP_1(':uuid')} exact component={EditSpotCard} />
+      <PrivateRoute path={APP_ROUTES.EDIT_PERSONA_UUID_STEP_1(':uuid')} exact component={EditPersonaCard} />
+      <PrivateRoute path={APP_ROUTES.EDIT_SPOT_UUID_STEP_2(':uuid')} exact component={EditSpotPage} />
+      <PrivateRoute path={APP_ROUTES.EDIT_PERSONA_UUID_STEP_2(':uuid')} exact component={EditPersonaPage} />
       <Route path={APP_ROUTES.LOGIN} exact component={Login} />
-      <Route path={APP_ROUTES.SPOT_CREATION_STEP_1} exact component={CreateSpotsStep1} />
-      <Route path={APP_ROUTES.SPOT_CREATION_STEP_2} exact component={CreateSpotsCard} />
-      <Route path={APP_ROUTES.SPOT_CREATION_STEP_3} exact component={CreateSpotsPage} />
+      <PrivateRoute path={APP_ROUTES.SPOT_CREATION_STEP_1} exact component={CreateSpotsStep1} />
+      <PrivateRoute path={APP_ROUTES.SPOT_CREATION_STEP_2} exact component={CreateSpotsCard} />
+      <PrivateRoute path={APP_ROUTES.SPOT_CREATION_STEP_3} exact component={CreateSpotsPage} />
       <Route path={APP_ROUTES.REGISTER} exact component={Register} />
       <PrivateRoute path={APP_ROUTES.PERSONA_CREATION_STEP_1} exact component={InitialStep} />
       <Route path={APP_ROUTES.PERSONA_CREATION_STEP_2} exact component={CreatePersonaCard} />
@@ -59,8 +59,8 @@ export const AppRouter: FunctionComponent = () => {
       <Route path={APP_ROUTES.CONTACT} exact component={Contact} />
       <Route path={APP_ROUTES.RESET_PASSWORD} exact component={ResetPassword} />
       <Route path={APP_ROUTES.CHANGE_PASSWORD} exact component={ChangePassword} />
-      <Route path={APP_ROUTES.SPOT_BOOK} exact component={SpotBook} />
-      <Route path={APP_ROUTES.CONTACT_BOOK} exact component={ContactBook} />
+      <PrivateRoute path={APP_ROUTES.SPOT_BOOK} exact component={SpotBook} />
+      <PrivateRoute path={APP_ROUTES.CONTACT_BOOK} exact component={ContactBook} />
       <Route path={APP_ROUTES.PERSONA_PREVIEW(':uuid')} exact component={PersonaPreview} />
       <Route path={APP_ROUTES.SPOT_PREVIEW(':uuid')} exact component={SpotPreview} />
     </Switch>
