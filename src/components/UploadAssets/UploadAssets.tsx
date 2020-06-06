4b998ebfb52b7ab1f4003815e4f6da0cbc886406
Lukasz Mazurowski <lukasz.mazurowski@gmail.com>
2020-06-06 16:39:15 +0200

    fixed styles

--- src/components/UploadAssets/UploadAssets.tsx
+++ src/components/UploadAssets/UploadAssets.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled from 'styled-components';
-import { Upload, Button } from 'antd';
+import { Upload as UploadAnt, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { UploadFile } from 'antd/lib/upload/interface';
+import { v4 } from 'uuid';
+
+const Upload = styled(UploadAnt)`
+  width: 100%;
+`;
 
 const UploadWrapper = styled.div`
   display: flex;
+  width: 100%;
   margin: 20px 0;
 `;
 
@@ -14,27 +20,46 @@ type UploadAssetsProps = {
   onRemoveFile(file: UploadFile): void;
 };
 
-export const UploadAssets: FC<UploadAssetsProps> = ({ onAddFile, onRemoveFile }: UploadAssetsProps) => (
-  <UploadWrapper>
-    <Upload
-      customRequest={(options) => {
-        onAddFile(options.file);
-      }}
-      progress={{
-        successPercent: 100,
-        status: 'success',
-        showInfo: false,
-      }}
-      previewFile={(file) => {
-        return Promise.resolve(file.type);
-      }}
-      onRemove={(file) => {
-        onRemoveFile(file);
-      }}
-    >
-      <Button>
-        <UploadOutlined /> Upload
-      </Button>
-    </Upload>
-  </UploadWrapper>
-);
+export const UploadAssets: FC<UploadAssetsProps> = ({ onAddFile, onRemoveFile }: UploadAssetsProps) => {
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+
+  return (
+    <UploadWrapper>
+      <Upload
+        listType="picture"
+        fileList={fileList}
+        customRequest={(options) => {
+          onAddFile(options.file);
+          const url = URL.createObjectURL(options.file);
+          const fileObj: UploadFile = {
+            size: options.file.size,
+            type: options.file.type,
+            uid: v4(),
+            name: options.file.name,
+            status: 'done',
+            url,
+            thumbUrl: url,
+          };
+          setFileList(fileList.concat([fileObj]));
+        }}
+        progress={{
+          successPercent: 100,
+        }}
+        previewFile={(file) => {
+          return Promise.resolve(file.type);
+        }}
+        onRemove={(file) => {
+          setFileList(fileList.filter((fileToRemove) => fileToRemove.uid !== file.uid));
+          onRemoveFile(file);
+          if (file.url) {
+            URL.revokeObjectURL(file.url);
+          }
+        }}
+      >
+        <Button>
+          <UploadOutlined /> Upload
+        </Button>
+      </Upload>
+    </UploadWrapper>
+  );
+};
