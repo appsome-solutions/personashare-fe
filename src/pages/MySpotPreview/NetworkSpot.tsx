4d457f99a5dcd5aff1249c0a7d698b363b265fbe
pumanitro <patrykjanik1710@gmail.com>
2020-06-16 22:02:42 +0200

    Fixed visibility and network data display for spot.

--- src/pages/MySpotPreview/NetworkSpot.tsx
+++ src/pages/MySpotPreview/NetworkSpot.tsx
@@ -1,17 +1,16 @@
 import React, { FC } from 'react';
 import { APP_ROUTES } from 'global/AppRouter/routes';
 import { useQuery } from '@apollo/react-hooks';
-import { gqlUser } from 'global/graphqls/schema';
-import { GET_USER } from 'global/graphqls/User';
 import { Overlay } from 'components/Overlay/Overlay';
 import { Spinner } from 'components/Spinner/Spinner';
 import { VisibilityOrNetwork } from '../../components/EntityPreview/VisibilityOrNetwork';
 import { GET_SPOT, GetCardType } from '../../global/graphqls/Spot';
+import { useParams } from 'react-router-dom';
 
 export const NetworkSpot: FC = () => {
-  const { data: userPersona } = useQuery<{ user: gqlUser }>(GET_USER);
+  const { uuid } = useParams();
   const { data, loading } = useQuery<GetCardType>(GET_SPOT, {
-    variables: { uuid: userPersona?.user?.defaultPersona },
+    variables: { uuid },
   });
 
   if (!data) return null;
