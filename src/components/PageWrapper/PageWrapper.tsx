1b85c7fd66c7c5954fd1efcc4dc898b83119e899
JanikOskar <oskar.janik@gmail.com>
2020-06-23 16:25:19 +0200

    register logo

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -7,6 +7,5 @@ export const PageWrapper = styled(Flex)<FlexboxProps>`
   flex-direction: ${(props) => props.flexDirection || 'column'};
   padding: 0 16px;
   background-color: ${(props) => props.theme.colors.utils.background.mid};
-
   overflow: auto;
 `;
