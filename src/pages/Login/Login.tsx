e5d0b11f620b68eaf1b6f5518b7d028a8632714a
pumanitro <patrykjanik1710@gmail.com>
2020-07-21 14:43:32 +0200

    Not redirecting on mobile phones.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -193,10 +193,12 @@ export const Login: FunctionComponent = () => {
 
         const idToken = await firebase?.getCurrentUser()?.getIdToken();
 
-        window.location.href = process.env.REACT_APP_BASE_URL || window.location.origin;
-
         if (idToken) {
           handleBEConnection(idToken);
+        }
+
+        if (window.innerWidth > 767) {
+          window.location.href = process.env.REACT_APP_BASE_URL || window.location.origin;
         }
       })
       .catch(() => {
